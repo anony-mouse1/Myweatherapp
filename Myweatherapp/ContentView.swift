03c894744842d1e76c2126efccac07a0cd185d16
anony-mouse1 <mindblowingmaths@gmail.com>
2022-07-19 10:17:19 -0700

    Initial Commit

---
+++ Myweatherapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Myweatherapp
+//
+//  Created by Fatimah Hussain on 7/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Myweatherapp/MyweatherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyweatherappApp.swift
+//  Myweatherapp
+//
+//  Created by Fatimah Hussain on 7/19/22.
+//
+
+import SwiftUI
+
+@main
+struct MyweatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
